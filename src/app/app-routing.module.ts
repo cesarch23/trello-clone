8870ff1cc7a55bbbea8f01388d050be064864823
Cesar Choccata <c21choccata@gmail.com>
2024-07-12 15:41:58 -0500

    Add app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { NotFoundPageComponent } from './shared/components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule)
+  },
+  {
+    path:'boards',
+    loadChildren:()=>import('./features/boards/boards.module').then(m=>m.BoardsModule)
+  },
+  {
+    path:'',
+    redirectTo:'boards',
+    //esto es mandatorio ojo si cambia a login
+    pathMatch:'full'
+  },
+  {
+    path:'404',
+    component:NotFoundPageComponent
+  },
+  {
+    path:'**',
+    component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
