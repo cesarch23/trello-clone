c75c2b8507c6d0622c8244d7e87d7eb9e2426c23
Cesar Choccata <c21choccata@gmail.com>
2024-07-17 22:56:43 -0500

    Add board component

--- src/app/features/workspaces/pages/board/board.component.ts
+++ src/app/features/workspaces/pages/board/board.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import  { faEllipsis} from '@fortawesome/free-solid-svg-icons/faEllipsis'
+import  { faPen} from '@fortawesome/free-solid-svg-icons/faPen'
+import  { faPlus} from '@fortawesome/free-solid-svg-icons/faPlus'
+import  { faFilm} from '@fortawesome/free-solid-svg-icons/faFilm'
 
 @Component({
   selector: 'app-board',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent {
+  faEllipsis = faEllipsis;
+  faPen = faPen;
+  faPlus = faPlus;
+  faFilm = faFilm;
 
+  array = [
+    1,2,3,4,5,6,7
+  ]
+
+  arr = [
+    1,2,3,4,5,6,7,8,9,
+    10,11,12,13,14,
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14
+  ]
 }
